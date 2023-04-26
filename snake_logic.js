db804b3a971cec2418cee4e754b30435e9a325fd
Casper-Pahkala <casper.pahkala@gmail.com>
2023-04-26 22:45:56 +0300

    update

--- snake_logic.js
+++ snake_logic.js
@@ -21,8 +21,38 @@ var gameStarted = false;
 var snakeSpeed = 120;
 const deathSound = new Audio('death_sound.mp3');
 const pointSound = new Audio('point_sound.mp3');
-const backgroundMusic = new Audio('background_music.mp3');
+var backgroundMusic = new Audio('background_music.mp3');
 let machineId = localStorage.getItem('MachineId');
+const themeSlider = document.getElementById('themeSlider');
+themeSlider.checked = true;
+var nightMode = true;
+
+themeSlider.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        themeToNight();
+    } else {
+        themeToLight();
+    }
+})  
+
+function themeToNight() {
+    document.body.style.backgroundColor = 'rgb(48, 48, 48)';
+    document.getElementsByClassName('heading')[0].style.color = 'whitesmoke';
+    document.getElementsByClassName('score')[0].style.color = 'whitesmoke';
+    document.getElementsByClassName('score')[1].style.color = 'whitesmoke';
+    document.getElementsByClassName('night-mode-text')[0].style.color = 'whitesmoke';
+    document.getElementsByClassName('snake-board')[0].style.borderColor = 'antiquewhite';
+}
+
+function themeToLight() {
+    document.body.style.backgroundColor = '#FFFAFA';
+    document.getElementsByClassName('heading')[0].style.color = 'rgb(48, 48, 48)';
+    document.getElementsByClassName('score')[0].style.color = 'rgb(48, 48, 48)';
+    document.getElementsByClassName('score')[1].style.color = 'rgb(48, 48, 48)';
+    document.getElementsByClassName('night-mode-text')[0].style.color = 'rgb(48, 48, 48)';
+    document.getElementsByClassName('snake-board')[0].style.borderColor = 'rgb(48, 48, 48)';
+}
+
 if (!machineId) {
     machineId = crypto.randomUUID();
     localStorage.setItem('MachineId', machineId);
@@ -34,15 +64,33 @@ if (!highScore) {
     highScore = 0;
 }
 highScoreText.innerHTML = 'High score: ' + highScore;
-const playButton = document.getElementById('playButton');
-playButton.onclick = function() {
+const easyButton = document.getElementById('easyButton');
+const mediumButton = document.getElementById('mediumButton');
+const hardButton = document.getElementById('hardButton');
+easyButton.onclick = easyMode;
+mediumButton.onclick = mediumMode;
+hardButton.onclick = hardMode;
+
+function easyMode() {
+    play(120);
+}
+function mediumMode() {
+    play(90);
+}
+function hardMode() {
+    play(60);
+}
+function play(speed) {
+    snakeSpeed = speed;
+    console.log(snakeSpeed);
     const playButtonContainer = document.getElementById('playButtonContainer');
     playButtonContainer.remove();
     const boardContainer = document.getElementById('boardContainer');
     boardContainer.style.display = 'block';
     startGame();
-
 }
+
+
 
 function startGame() {
     backgroundMusic.play();
@@ -54,10 +102,16 @@ function startGame() {
     currentY = 0;
     moveDirection = "right";
     GameOver = false;
-    gameStarted = true;
+    
     spawnHead();
     spawnBody();
+    spawnBody();
     spawnFruit();
+    //Snake movement
+    if (!gameStarted) {
+        setInterval(moveHead, snakeSpeed);        
+    }
+    gameStarted = true;
 }
 
 function spawnHead() {
@@ -121,8 +175,10 @@ function moveObjects() {
 
     //Check if head is inside snake bodies
     for (var i = 1; i <= snakeBodies.length; i++){
-        if (headPositions[i][0] == currentX && headPositions[i][1] == currentY) {
-            gameOver();
+        if (headPositions[i] != undefined) {
+            if (headPositions[i][0] == currentX && headPositions[i][1] == currentY) {
+                gameOver();
+            }
         }
     }
     if (currentX == currentFruitPosition[0] && currentY == currentFruitPosition[1]) {
@@ -144,7 +200,9 @@ function moveObjects() {
         }
         //Else move the right body part to the corresponding latest head position
         else {
-            snakeBodies[i].style.transform = `translateY(${headPositions[i + 1][1]}px) translateX(${headPositions[i + 1][0]}px)`;
+            if (headPositions[i + 1] != undefined) {
+                snakeBodies[i].style.transform = `translateY(${headPositions[i + 1][1]}px) translateX(${headPositions[i + 1][0]}px)`;
+            }
         }
     }
 }
@@ -161,9 +219,7 @@ function eatFruit() {
         request(data).then((result) => {
         });
     }
-    
-    snakeSpeed += 2;
-    const pointSound = new Audio('point_sound.mp3');
+        const pointSound = new Audio('point_sound.mp3');
     pointSound.play();
     scoreText.innerHTML = 'Score: ' + score;
     spawnBody();
@@ -199,8 +255,8 @@ function spawnFruit() {
 
 function gameOver() {
     score = 0;
-    snakeSpeed = 120;
     backgroundMusic.pause();
+    backgroundMusic = new Audio('background_music.mp3');
     deathSound.play();
     GameOver = true;
     playAgainButton.style.display = 'block';
@@ -250,7 +306,3 @@ function moveHead() {
         moveObjects();
     }
 }
-
-//Snake movement
-setInterval(moveHead, snakeSpeed);
-
